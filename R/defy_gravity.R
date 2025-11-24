4576015d953f3426d8dd77a53e51a5cde4c9cb70
Ashley Mullan <ashleymullan2000@gmail.com>
2025-11-24 11:50:34 -0500

    update filter and arrange

---
+++ R/defy_gravity.R
@@ -0,0 +1,17 @@
+#' @import dplyr
+NULL
+
+#' Organize your rows so they defy gravity!
+#'
+#' Pick a variable to sort by, and let your data frame defy gravity! (arrange equivalent)
+#'
+#' @param .data A data frame or tibble
+#' @param ... other arguments you'd probably pass to the arrange function
+#' @return A data frame sorted on a specific variable
+#' @export
+#' @examples
+#' data <- read.csv(data_link)
+#' data |> defy_gravity(swankifiedness) #
+defy_gravity <- function(.data, ...) {
+  dplyr::arrange(.data, ...)
+}
